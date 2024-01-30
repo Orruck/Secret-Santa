bd05cd58d451d0def9311cebd6075de89704694f
John <46977252+Orruck@users.noreply.github.com>
2024-01-29 20:43:40 -0500

    fix randomization algorithm to avoid duplicate items
    Previously, the randomization algorithm occasionally resulted in the
    same item appearing at the same index in both the original and
    randomized arrays. This commit addresses the issue by introducing a
    check to ensure that the randomized array does not have duplicates
    at the same index as the original array.

--- src/shuffle.js
+++ src/shuffle.js
@@ -1,6 +1,6 @@
 function shuffle(array) {
     let randomizedArray = [];
-    let copyOfArray = array;
+    let copyOfArray = array.slice();
     let currentIndex = copyOfArray.length;
     let randomIndex;
 
@@ -12,7 +12,29 @@ function shuffle(array) {
         currentIndex--;
     }
 
+    randomizedArray = reshuffle(array, randomizedArray)
+
     return randomizedArray;
+}
+
+function reshuffle(array, arrayCopy) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === arrayCopy[i]){
+            console.log(`${array[i]} ${arrayCopy[i]} "EQUALS"`);
+            console.log(array)
+            console.log(arrayCopy)
+            
+            // removeIndex, push removedIndex into randomIndex
+            let currentArrayItem = arrayCopy[i];
+            arrayCopy.splice(i, 1);
+
+            arrayCopy.splice((arrayCopy.length+1) * Math.random() | 0, 0, currentArrayItem);
+            
+            i = 0;
+        }
+    }
+
+    return arrayCopy;
 }
 
 export { shuffle }
